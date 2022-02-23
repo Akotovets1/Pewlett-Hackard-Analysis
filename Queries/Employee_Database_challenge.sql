34018d168c29d7beccf8eef93d57395c4dcabd08
Andrew Kotovets <akotovets@gmail.com>
2022-02-23 12:40:42 -0800

    Adding additional files

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,5 +29,21 @@ FROM unique_titles AS ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
 
--- DELIVERABLE 2
-
+-- DELIVERABLE 2 Create Mentor List
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	   e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibilty
+FROM employees as e
+	INNER JOIN dept_emp as de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+		ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (t.to_date = '9999-01-01')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no ASC, de.from_date DESC;
